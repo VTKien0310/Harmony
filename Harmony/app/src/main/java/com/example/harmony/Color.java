ff75ebcfb0e2fcbf8545f550d50e3c897bd74409
Sky1ight <kiennu0987@gmail.com>
2020-06-12 21:18:52 +0700

    Add base lines for Color and Scheme classes

---
+++ Harmony/app/src/main/java/com/example/harmony/Color.java
@@ -0,0 +1,22 @@
+package com.example.harmony;
+
+class Color {
+    private int red;
+    private int green;
+    private int blue;
+
+    public Color getOpposite()
+    {
+
+    }
+
+    public Color getNext()
+    {
+
+    }
+
+    public Color getLeap()
+    {
+        
+    }
+}
---
+++ Harmony/app/src/main/java/com/example/harmony/Scheme.java
@@ -0,0 +1,25 @@
+package com.example.harmony;
+
+class Scheme {
+    private Color[] colors;
+
+    public Color getColorAtPos(int pos)
+    {
+
+    }
+
+    public void setColorAtPos(Color color)
+    {
+
+    }
+
+    public void generateColors()
+    {
+
+    }
+
+    public void save()
+    {
+
+    }
+}
